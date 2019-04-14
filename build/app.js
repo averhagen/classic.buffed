42cda72a0be74063794fabddce2cb6b4f93222e6
amverhagen <averhagen@users.noreply.github.com>
2019-04-14 18:15:20 -0400

    Change app.js to import data_connection statement to use the side effect import convention.

--- build/app.js
+++ build/app.js
@@ -1,7 +1,14 @@
 "use strict";
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
-var connection = require("./models/data_connection");
+Promise.resolve().then(function () { return __importStar(require('./models/data_connection')); });
 // Create a new express application instance
 var app = express();
 app.get('/', function (req, res) {
@@ -10,4 +17,3 @@ app.get('/', function (req, res) {
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
-connection.default;
