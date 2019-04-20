fa6bdcf7bbfccbcdf5d73e16ae312d4a14ec6461
amverhagen <averhagen@users.noreply.github.com>
2019-04-20 18:40:46 -0400

    Change attribute name '__MONGO_DB_NAME' to '__MONGO_DB_NAME__' inside test_env object.

--- app/vendor.d.ts
+++ app/vendor.d.ts
@@ -2,7 +2,7 @@ declare module NodeJS {
     interface Global {
         test_env: {
             __MONGO_URI__: string,
-            __MONGO_DB_NAME: string
+            __MONGO_DB_NAME__: string
         }
     }
 }
