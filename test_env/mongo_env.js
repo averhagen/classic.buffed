3e857e36f1febf06e8fbdeccbbb7c6c5f837a2b2
amverhagen <averhagen@users.noreply.github.com>
2019-04-20 13:47:31 -0400

    Cange mongo_env.js to environment file provided at 'https://jestjs.io/docs/en/mongodb'.

--- test_env/mongo_env.js
+++ test_env/mongo_env.js
@@ -1,10 +1,37 @@
+// mongo-env.js
 const NodeEnvironment = require('jest-environment-node');
 
-class MongooseEnvironment extends NodeEnvironment {
-    constructor(config, context) {
-        super(config, context);
-        console.log("We made it boys.")
-    }
+const path = require('path');
+
+const fs = require('fs');
+
+const globalConfigPath = path.join(__dirname, 'globalConfig.json');
+
+class MongoEnvironment extends NodeEnvironment {
+  constructor(config) {
+    super(config);
+  }
+
+  async setup() {
+    console.log('Setup MongoDB Test Environment');
+
+    const globalConfig = JSON.parse(fs.readFileSync(globalConfigPath, 'utf-8'));
+
+    this.global.__MONGO_URI__ = globalConfig.mongoUri;
+    this.global.__MONGO_DB_NAME__ = globalConfig.mongoDBName;
+
+    await super.setup();
+  }
+
+  async teardown() {
+    console.log('Teardown MongoDB Test Environment');
+
+    await super.teardown();
+  }
+
+  runScript(script) {
+    return super.runScript(script);
+  }
 }
 
-module.exports = MongooseEnvironment;
+module.exports = MongoEnvironment;
