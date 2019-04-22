b0c5b378165009c46364dc887bab3e781e14351c
amverhagen <averhagen@users.noreply.github.com>
2019-04-22 19:58:18 -0400

    Change buff.ts to export a mongoose buff model instead of exporting just the mongoose buff schema.

--- build/models/buff.js
+++ build/models/buff.js
@@ -1,8 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var mongoose = require("mongoose");
+var buffCollectionName = "buff";
+var nameFieldOptions = { type: String, required: true };
+var rankFieldOptions = { type: Number, required: true };
 var buffSchema = new mongoose.Schema({
-    name: String,
-    rank: Number,
-}, { collection: 'buff' });
+    name: nameFieldOptions,
+    rank: rankFieldOptions,
+}, { collection: buffCollectionName });
 exports.buffSchema = buffSchema;
+var BuffModel = mongoose.model(buffCollectionName, buffSchema);
+exports.BuffModel = BuffModel;
