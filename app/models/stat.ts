6b80c33b90b7ce622a099bb3158da126e2afd3c3
amverhagen <averhagen@users.noreply.github.com>
2019-04-23 17:11:58 -0400

    Rename 'statSchemaDefinition' to 'schemaDefintion' inside 'stat.ts'.

--- app/models/stat.ts
+++ app/models/stat.ts
@@ -1,14 +1,15 @@
 import mongoose = require("mongoose");
 
 const collectionName: string = "stat";
+
 const nameFieldOptions = { type: String, required: true };
 
-const statSchemaDefinition: mongoose.SchemaDefinition = {
+const schemaDefinition: mongoose.SchemaDefinition = {
     name: nameFieldOptions
 };
 
 const statSchema = new mongoose.Schema(
-    statSchemaDefinition,
+    schemaDefinition,
     { collection: collectionName }
 );
 
