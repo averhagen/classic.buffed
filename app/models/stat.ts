50e86ced8051bf4d8622232e3e0d86dd9e12f887
amverhagen <averhagen@users.noreply.github.com>
2019-04-23 19:23:44 -0400

    Move code in 'stat.ts' for readability purposes.

--- app/models/stat.ts
+++ app/models/stat.ts
@@ -3,14 +3,14 @@ import mongoose = require("mongoose");
 const collectionName: string = "stat";
 const nameFieldOptions = { type: String, required: true };
 
+interface StatDocument extends mongoose.Document {
+    name: typeof nameFieldOptions.type
+}
+
 const statSchema = new mongoose.Schema(
     { name: nameFieldOptions },
     { collection: collectionName }
 );
-
-interface StatDocument extends mongoose.Document {
-    name: typeof nameFieldOptions.type
-}
 
 const statModel = mongoose.model<StatDocument>(collectionName, statSchema);
 
