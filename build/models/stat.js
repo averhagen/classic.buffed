f89a605b4926fee59e1141d9bf0a4773d91ec255
amverhagen <averhagen@users.noreply.github.com>
2019-04-24 17:16:51 -0400

    Run tsc compiler on stat.ts

--- build/models/stat.js
+++ build/models/stat.js
@@ -1,7 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var mongoose = require("mongoose");
-var statSchema = new mongoose.Schema({
-    name: String,
-}, { collection: 'stat' });
+var collectionName = "stat";
+var nameFieldOptions = { type: String, required: true };
+var statSchema = new mongoose.Schema({ name: nameFieldOptions }, { collection: collectionName });
 exports.statSchema = statSchema;
+var statModel = mongoose.model(collectionName, statSchema);
+exports.statModel = statModel;
