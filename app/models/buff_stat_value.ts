27a70617fa807dbd2214c855dd8b80a26414b8ed
amverhagen <averhagen@users.noreply.github.com>
2019-04-24 17:15:02 -0400

    Update buff_stat_value.ts to be more staticly typed.

--- app/models/buff_stat_value.ts
+++ app/models/buff_stat_value.ts
@@ -1,9 +1,22 @@
 import mongoose = require('mongoose');
+import { BuffModel } from './buff';
+import { statModel } from './stat';
+
+const collectionName: string = "buff_stat_value";
+const valueFieldOptions = { type: Number };
+const buffFieldOptions = { type: mongoose.Schema.Types.ObjectId, ref: BuffModel.modelName };
+const statFieldOptions = { type: mongoose.Schema.Types.ObjectId, ref: statModel.modelName };
+
+interface BuffStatValueDocument extends mongoose.Document {
+    value: typeof valueFieldOptions.type,
+    buff: typeof buffFieldOptions.type,
+    stat: typeof statFieldOptions.type
+}
 
 const buffStateValueSchema = new mongoose.Schema({
-    value: Number,
-    buff: { type: mongoose.Schema.Types.ObjectId, ref: 'buff' },
-    stat: { type: mongoose.Schema.Types.ObjectId, ref: 'stat' }
+    value: valueFieldOptions,
+    buff: buffFieldOptions,
+    stat: statFieldOptions
 });
 
-export const BuffStatValue = mongoose.model('buff_stat_value', buffStateValueSchema);
+export const BuffStatValue = mongoose.model<BuffStatValueDocument>(collectionName, buffStateValueSchema);
