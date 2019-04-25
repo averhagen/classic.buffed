687cc29575fa4cbb9197d85d1605ca5b46adfe3f
amverhagen <averhagen@users.noreply.github.com>
2019-04-25 18:11:43 -0400

    Change method 'addNewBuff' in buff_controller to 'createBuff'. Change method 'createBuff' to use async await syntax instead of promise syntax.

--- app/controllers/buff_controller.ts
+++ app/controllers/buff_controller.ts
@@ -4,15 +4,23 @@ import { BuffModel } from "../models/buff";
 
 export class BuffController {
 
-    public createBuff(req: Request, res: Response) {
+    public async createBuff(req: Request, res: Response) {
         console.log("Received buff post request: " + req.url);
-        const newBuff = new BuffModel({ name: req.query["name"], rank: req.query["rank"] });
-        newBuff.save((err, contact) => {
-            if (err) {
-                res.send(err);
-            }
-            res.json(contact);
-        });
+        const buffValues = this.extractBuffValuesFromReq(req);
+
+        try {
+            const buffDocument = await new BuffModel(buffValues);
+            res.json(buffDocument);
+        } catch (error) {
+            res.send(error);
+        }
+    }
+
+    private extractBuffValuesFromReq(req: Request): any {
+        return {
+            name: req.query["name"],
+            rank: req.query["rank"]
+        }
     }
 
     public getBuffs(req: Request, res: Response) {
