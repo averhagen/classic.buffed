84b5b71edb213532db7bc6bd06436241e0668149
amverhagen <averhagen@users.noreply.github.com>
2019-04-25 17:08:46 -0400

    Write test that checks that a buff can be saved and the saved values match the input values.

--- app/models/__TESTS__/buff.test.ts
+++ app/models/__TESTS__/buff.test.ts
@@ -19,9 +19,19 @@ test("That saving a Buff without a Rank throws an error.", async () => {
     expect(buffDocument.save()).rejects.not.toBeNull();
 });
 
-
 test("That saving a Buff without a Rank or Name throws an error", async () => {
     const docWithoutRankOrBuffAttribute = {};
     const buffDocument = new BuffModel(docWithoutRankOrBuffAttribute);
     expect(buffDocument.save()).rejects.not.toBeNull();
+});
+
+test("That valid buff is saved with the proper values.", async () => {
+    const buffName: String = "Divine Spirit";
+    const buffRank: Number = 1;
+
+    const docWithBuffValues = { name: buffName, rank: buffRank };
+    const savedBuff = await new BuffModel(docWithBuffValues).save();
+
+    expect(savedBuff.name).toBe(buffName);
+    expect(savedBuff.rank).toBe(buffRank);
 });
