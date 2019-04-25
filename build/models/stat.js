b107ee31d426d863b9e5bde3141ea5c2aa0dbf0a
amverhagen <averhagen@users.noreply.github.com>
2019-04-25 19:45:34 -0400

    Remove tracking of compiled build javascript files.

--- build/models/stat.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var mongoose = require("mongoose");
-var collectionName = "stat";
-var nameFieldOptions = { type: String, required: true };
-var statSchema = new mongoose.Schema({ name: nameFieldOptions }, { collection: collectionName });
-exports.statSchema = statSchema;
-var statModel = mongoose.model(collectionName, statSchema);
-exports.statModel = statModel;
