26ab75516daabdfc46324094d7bc14e760e36421
amverhagen <averhagen@users.noreply.github.com>
2019-04-25 16:39:35 -0400

    Write test that checks that savinga  buff without a name throws an error.

--- app/models/__TESTS__/buff.test.ts
+++ app/models/__TESTS__/buff.test.ts
@@ -1,7 +1,14 @@
 import * as connection_utils from "../../../test_env/utils/connection_utils";
+import { BuffModel, BuffDocument } from "../buff";
 
 beforeAll(connection_utils.startConnectionToTestDB);
 
 afterAll(connection_utils.stopConnectionToTestDB);
 
 connection_utils.testConnectionIsValid();
+
+test("That saving a Buff without a name throws an error.", async () => {
+    const docWithoutNameAttribute = { rank: 2 };
+    const abc = new BuffModel(docWithoutNameAttribute);
+    expect(abc.save()).rejects.not.toBeNull();
+});
