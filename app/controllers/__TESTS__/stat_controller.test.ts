77dc2cf6e653939ebfa0d858ee6403f1e8a09957
amverhagen <averhagen@users.noreply.github.com>
2019-05-21 20:33:05 -0400

    Write test file for stat_controller.

--- app/controllers/__TESTS__/stat_controller.test.ts
+++ app/controllers/__TESTS__/stat_controller.test.ts
@@ -1,4 +1,34 @@
 import { startConnectionToTestDB, stopConnectionToTestDB } from "../../test_utils/connection_utils";
+import { statModel } from "../../models/stat";
+import { StatController } from "../stat_controller";
 
 beforeAll(startConnectionToTestDB);
 afterAll(stopConnectionToTestDB);
+
+test("Creating a stat makes the correct stat based on the request", async () => {
+
+    const statName: string = "Spirit";
+
+    const req: any = {
+        query: {
+            name: statName
+        },
+    };
+
+    const res: any = {
+        json: jest.fn(),
+        send: jest.fn()
+    };
+
+    expect(await statModel.findOne()).toBeNull();
+
+    const statController = new StatController();
+
+    await statController.createStat(req, res);
+
+    expect(res.json).toBeCalled();
+    expect(res.send).not.toBeCalled();
+
+    const createdStat: any = await statModel.findOne().exec();
+    expect(createdStat.name).toEqual(statName);
+});
