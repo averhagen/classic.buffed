a95f5b280767a46d9231eaad3ce5303eb2a5d9d9
amverhagen <averhagen@users.noreply.github.com>
2019-07-02 21:57:17 -0400

    Add a test to stat.test.ts that checks that an error is thrown when trying to save an empty stat document.

--- app/models/__TESTS__/stat.test.ts
+++ app/models/__TESTS__/stat.test.ts
@@ -1,3 +1,4 @@
+import mongoose = require("mongoose");
 import * as connection_utils from "../../test_utils/connection_utils";
 import { statModel } from "../stat";
 
@@ -6,3 +7,14 @@ beforeAll(async () => { await connection_utils.startConnectionToTestDB(); });
 afterAll(async () => { await connection_utils.stopConnectionToTestDB(); });
 
 connection_utils.testConnectionIsValid();
+
+test("StatDocument.save() throws an error when trying to save a stat document with values.", async () => {
+    let thrownError;
+    try {
+        await new statModel({}).save();
+    } catch(error) {
+        thrownError = error;
+    }
+    expect(thrownError).not.toBeNull();
+    expect(thrownError).toBeInstanceOf(mongoose.Error.ValidationError);
+});
