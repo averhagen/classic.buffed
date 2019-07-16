df3e60febeb196d039c1ee73b4c98ad8bf1fdfc9
amverhagen <averhagen@users.noreply.github.com>
2019-07-15 20:22:57 -0400

    Create route and pug file to view all buffs.

--- app/routes/buffs.ts
+++ app/routes/buffs.ts
@@ -1,10 +1,12 @@
 import express = require('express');
+import { BuffModel } from '../models/buff';
 
 const buffRouter = express.Router();
 
 buffRouter.get('/', async (req, res, next) => {
     console.log("buff get called");
-    res.send("NOT YET IMPLEMENTED");
+    const buffs = await BuffModel.find().exec();
+    res.render('buffs/view_all_buffs', { buffs: buffs });
 });
 
 export { buffRouter };
