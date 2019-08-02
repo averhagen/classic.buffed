1c828e9cff76b5d87a4acdba8a20d53b41b4126f
amverhagen <averhagen@users.noreply.github.com>
2019-08-01 20:02:25 -0400

    Implement StatController.editStat() function.

--- app/controllers/rest/stat_controller.ts
+++ app/controllers/rest/stat_controller.ts
@@ -16,6 +16,19 @@ export class StatController {
 
     public async editStat(req: Request, res: Response, next: NextFunction) {
         console.log("Received edit stat REST request.");
+        try {
+            const foundStat = await statModel.findOne({ _id: req.query._id });
+            if (foundStat) {
+                if (req.query.name) {
+                    foundStat.name = req.query.name;
+                }
+                res.send(await foundStat.save());
+            } else {
+                res.status(404).send("Unable to find stat with given ID");
+            }
+        } catch (error) {
+            next(error);
+        }
     }
 
     public async deleteStat(req: Request, res: Response, next: NextFunction) {
