50ce1e75d283187ca84a8e793956c6053942eec6
amverhagen <averhagen@users.noreply.github.com>
2019-08-03 16:11:22 -0400

    Route GET request '/rest/statbuffer/' to StatBufferController.buffStats().

--- app/routes/rest.ts
+++ app/routes/rest.ts
@@ -2,6 +2,7 @@ import express = require('express');
 import { BuffController } from '../controllers/rest/buff_controller';
 import { StatController } from '../controllers/rest/stat_controller';
 import { BuffStatValueController } from '../controllers/rest/buff_stat_value_controller';
+import { StatBufferController } from '../controllers/rest/stat_buffer_controller';
 
 const restRouter = express.Router();
 
@@ -22,5 +23,9 @@ restRouter.put("/stats", statController.editStat);
 const buffStatValueController = new BuffStatValueController();
 restRouter.get("/buffstatvalues", buffStatValueController.getBuffStatValue);
 restRouter.post("/buffstatvalues", buffStatValueController.addNewBuffStatValue);
+
+// REST routes for stat_buffer
+const statBufferController = new StatBufferController();
+restRouter.get("/statbuffer", statBufferController.buffStats);
 
 export { restRouter };
