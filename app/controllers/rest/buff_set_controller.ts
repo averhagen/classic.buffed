760384a5341c72b143198a1d9c14cbd38a8093be
amverhagen <averhagen@users.noreply.github.com>
2019-08-05 13:44:19 -0400

    Update BuffSetController.getBuffSet() to delegate logic to the BuffSet class.

--- app/controllers/rest/buff_set_controller.ts
+++ app/controllers/rest/buff_set_controller.ts
@@ -1,19 +1,27 @@
 import { NextFunction, Request, Response } from "express";
+import { BuffSet } from "../../models/buff_set";
 
 export class BuffSetController {
 
     public async getBuffSet(req: Request, res: Response, next: NextFunction) {
         console.log(req.query.buff_id);
         const selectedBuffs = req.query.buff_id;
-        let output: String = "You've found the StatBuffer <br>";
-        if(selectedBuffs) {
-            if(Array.isArray(selectedBuffs)) {
-                selectedBuffs as Array<any>;
-                selectedBuffs.forEach(async (value) => {
-                    output += value +"<br>";
-                });
+
+        try {
+            if (selectedBuffs) {
+                const buffSet = new BuffSet();
+                if (Array.isArray(selectedBuffs)) {
+                    selectedBuffs as Array<any>;
+                    selectedBuffs.forEach(async (buff_id) => {
+                        buffSet.addBuffByID(buff_id);
+                    });
+                }
+                return res.send(buffSet.getStatDifferentials());
+            } else {
+                throw new Error("Invalid input");
             }
+        } catch (error) {
+            next(error);
         }
-        res.send(output);
     }
 }
