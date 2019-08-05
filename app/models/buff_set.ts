ff4b0e8bd59c22df695b92570821fc8f388fbfe8
amverhagen <averhagen@users.noreply.github.com>
2019-08-05 17:50:24 -0400

    Update BuffSet.getStatDifferentials() to return an array of StatDifferentials based on the buffs in the set.

--- app/models/buff_set.ts
+++ app/models/buff_set.ts
@@ -1,44 +1,47 @@
-import { BuffDocument, BuffModel } from "./buff";
+import { BuffStatValue } from "./buff_stat_value";
+import { StatDocument } from "./stat";
 import { StatDifferential } from "./stat_differential";
 
 export class BuffSet {
 
-    buffs: Set<BuffDocument>;
-    buff_ids: Array<String>;
+    buff_ids: Set<String>;
 
     constructor() {
-        this.buffs = new Set();
-        this.buff_ids = new Array();
+        this.buff_ids = new Set();
     }
 
-    public addBuff(buff: BuffDocument): boolean {
-        this.buffs.add(buff);
-        this.buff_ids.push(buff._id);
-        return true;
+    public addBuffByID(buff_id: String): boolean {
+        this.buff_ids.add(buff_id);
+        return this.buff_ids.has(buff_id);
     }
 
-    public async addBuffByID(buff_id: String) {
-        const buff = await BuffModel.findOne({ _id: buff_id }).exec();
-        if (buff) {
-            this.addBuff(buff);
-        } else {
-            throw new Error("Invalid buff_id: " + buff_id);
+    public removeBuffByID(buff_id: String) {
+        this.buff_ids.delete(buff_id);
+        return this.buff_ids.has(buff_id);
+    }
+
+    public async getStatDifferentials(): Promise<Array<StatDifferential>> {
+        const buffStatValues = await BuffStatValue.find({ buff: { $in: Array.from(this.buff_ids.values()) } }).populate("stat").exec();
+
+        const statDiffMap: Map<StatDocument, StatDifferential> = new Map();
+
+        for (let i = 0; i < buffStatValues.length; i++) {
+            const currentBuffStatValue = buffStatValues[i];
+            const currentStatDifferential = statDiffMap.get(currentBuffStatValue.stat);
+            if (currentStatDifferential) {
+                currentStatDifferential.difference += currentBuffStatValue.value;
+            } else {
+                const newStatDifferential: StatDifferential = {
+                    stat: currentBuffStatValue.stat,
+                    difference: currentBuffStatValue.value
+                }
+                statDiffMap.set(newStatDifferential.stat, newStatDifferential);
+            }
         }
-    }
-
-    public removeBuff(buff: BuffDocument): boolean {
-        return false;
-    }
-
-    public removeBuffByID(buff_id: String): boolean {
-        return false;
-    }
-
-    public getStatDifferentials(): Array<StatDifferential> {
-        return new Array<any>();
+        return Array.from(statDiffMap.values());
     }
 
     public getBuffIDs(): Array<String> {
-        return this.buff_ids;
+        return Array.from(this.buff_ids.values());
     }
 }
