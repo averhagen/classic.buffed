35214defa7c6cda8296daad271f586b2accdfa8a
amverhagen <averhagen@users.noreply.github.com>
2019-08-05 13:15:20 -0400

    Update BuffSetController.getBuffSet(req, res, next) to expect an incoming array of buffs with the query name buff_id.

--- app/controllers/rest/buff_set.ts
+++ app/controllers/rest/buff_set.ts
@@ -3,28 +3,17 @@ import { NextFunction, Request, Response } from "express";
 export class BuffSetController {
 
     public async getBuffSet(req: Request, res: Response, next: NextFunction) {
-        console.log(req.query.selectedBuff);
-        if(req.query.selectedBuff) {
-            const selectedBuffs = req.query.selectedBuff;
+        console.log(req.query.buff_id);
+        const selectedBuffs = req.query.buff_id;
+        let output: String = "You've found the StatBuffer <br>";
+        if(selectedBuffs) {
             if(Array.isArray(selectedBuffs)) {
                 selectedBuffs as Array<any>;
                 selectedBuffs.forEach(async (value) => {
-                    if(value instanceof String) {
-                        value as String;
-                    }
+                    output += value +"<br>";
                 });
             }
         }
-        let output: String = "You've found the StatBuffer <br>";
-        output = output + req.query.stats + "<br>";
-        output = output + req.query.selectedBuffs.length;
-        output += req.query.selectedBuffs + "<br>";
-        output += req.query.selectedBuffs.length;
-        const buffs = req.query.selectedBuffs;
-        if(Array.isArray(buffs)) {
-            
-        }
-        if(output)
         res.send(output);
     }
 }
