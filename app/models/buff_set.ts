188670f825a00a2511aeb9f5c04a996037b29199
amverhagen <averhagen@users.noreply.github.com>
2019-08-05 14:54:23 -0400

    Implement addBuffByID() function in BuffSet class.

--- app/models/buff_set.ts
+++ app/models/buff_set.ts
@@ -1,20 +1,29 @@
-import { BuffDocument } from "./buff";
+import { BuffDocument, BuffModel } from "./buff";
 import { StatDifferential } from "./stat_differential";
 
 export class BuffSet {
 
     buffs: Set<BuffDocument>;
+    buff_ids: Array<String>;
 
     constructor() {
         this.buffs = new Set();
+        this.buff_ids = new Array();
     }
 
     public addBuff(buff: BuffDocument): boolean {
-        return false;
+        this.buffs.add(buff);
+        this.buff_ids.push(buff._id);
+        return true;
     }
 
-    public addBuffByID(buff_id: String): boolean {
-        return false;
+    public async addBuffByID(buff_id: String) {
+        const buff = await BuffModel.findOne({ _id: buff_id }).exec();
+        if (buff) {
+            this.addBuff(buff);
+        } else {
+            throw new Error("Invalid buff_id: " + buff_id);
+        }
     }
 
     public removeBuff(buff: BuffDocument): boolean {
@@ -27,5 +36,9 @@ export class BuffSet {
 
     public getStatDifferentials(): Array<StatDifferential> {
         return new Array<any>();
+    }
+
+    public getBuffIDs(): Array<String> {
+        return this.buff_ids;
     }
 }
