01cdd31b9d760bead20c3cbb84cca3e2f01a8d48
amverhagen <averhagen@users.noreply.github.com>
2019-08-05 17:52:10 -0400

    Update BuffSetController.getBuffSet() to make proper use of async/await.

--- app/controllers/rest/buff_set_controller.ts
+++ app/controllers/rest/buff_set_controller.ts
@@ -4,7 +4,7 @@ import { BuffSet } from "../../models/buff_set";
 export class BuffSetController {
 
     public async getBuffSet(req: Request, res: Response, next: NextFunction) {
-        console.log(req.query.buff_id);
+        console.log("Get Buff Set called.")
         const selectedBuffs = req.query.buff_id;
 
         try {
@@ -12,11 +12,13 @@ export class BuffSetController {
                 const buffSet = new BuffSet();
                 if (Array.isArray(selectedBuffs)) {
                     selectedBuffs as Array<any>;
-                    selectedBuffs.forEach(async (buff_id) => {
+                    selectedBuffs.forEach((buff_id) => {
                         buffSet.addBuffByID(buff_id);
                     });
+                } else {
+                    buffSet.addBuffByID(selectedBuffs);
                 }
-                return res.send(buffSet.getStatDifferentials());
+                return res.send(await buffSet.getStatDifferentials());
             } else {
                 throw new Error("Invalid input");
             }
