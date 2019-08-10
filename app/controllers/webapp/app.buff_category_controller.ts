f2d061fc55a5cfa8061ecd2091eacb99b9b2d45d
amverhagen <averhagen@users.noreply.github.com>
2019-08-10 19:34:06 -0400

    Implement WebAppBuffCategoryController.renderCreateBuffCategoryPage.

--- app/controllers/webapp/app.buff_category_controller.ts
+++ app/controllers/webapp/app.buff_category_controller.ts
@@ -1,4 +1,10 @@
+import { Request, Response, NextFunction } from 'express';
+import { BuffCategoryModel } from '../../models/buff_category';
 
 export class WebAppBuffCategoryController {
 
+    public async renderCreateBuffCategoryPage(req: Request, res: Response, next: NextFunction) {
+        const buffCategories = await BuffCategoryModel.find().exec();
+        res.render('buff_category/create_buff_category.pug', { buff_categories: buffCategories });
+    }
 }
