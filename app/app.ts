1651e0835de633faf33359ca3e50b7a909a22754
amverhagen <averhagen@users.noreply.github.com>
2019-08-24 21:43:14 -0400

    Update app.ts to send buff categories to index page.

--- app/app.ts
+++ app/app.ts
@@ -9,6 +9,7 @@ import { statRouter } from './routes/webapp/stats';
 import { buffCategoryRouter } from './routes/webapp/buff_category';
 import { statCategoryRouter } from './routes/webapp/stat_category';
 import { StatCategoryModel } from './models/stat_category';
+import { BuffCategoryModel } from './models/buff_category';
 
 // Create a new express application instance
 const app: express.Application = express();
@@ -30,7 +31,8 @@ app.get('/index', async function (req, res) {
     const buffs = await BuffModel.find().exec();
     const stats = await statModel.find().exec();
     const statCategories = await StatCategoryModel.find().exec();
-    res.render('index', { title: "Classic.Buffed", stats: stats, stat_categories: statCategories, buffs: buffs });
+    const buffCategories = await BuffCategoryModel.find().exec();
+    res.render('index', { title: "Classic.Buffed", stats: stats, stat_categories: statCategories, buffs: buffs, buff_categories: buffCategories });
   } catch (error) {
     console.log(error);
   }
